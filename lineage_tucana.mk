65fc8ad84139832eb1e12b79d65ffe397ebd4415
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-15 13:57:02 +0000

    tucana-user 9 PKQ1.190302.001 V11.0.5.0.PFDEUXM release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_tucana.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from tucana device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := xiaomi
+PRODUCT_DEVICE := tucana
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_NAME := lineage_tucana
+PRODUCT_MODEL := Mi Note 10
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := tucana
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="tucana-user 9 PKQ1.190302.001 V11.0.5.0.PFDEUXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/tucana_eea/tucana:9/PKQ1.190302.001/V11.0.5.0.PFDEUXM:user/release-keys
